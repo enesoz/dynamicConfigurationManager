0c0c5f2e0a2fd6c106b2b4ff10a4af72eb2a2bb3
eo9216 <eo9216@ACR5-517.aler.net>
2018-01-05 12:09:22 +0300

    overried equals and hascode remove wrong test

--- src/main/java/dynamic/configuration/manager/entity/ConfigurationEntity.java
+++ src/main/java/dynamic/configuration/manager/entity/ConfigurationEntity.java
@@ -82,4 +82,53 @@ public class ConfigurationEntity {
 		this.appName = appName;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (active ? 1231 : 1237);
+		result = prime * result + ((appName == null) ? 0 : appName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConfigurationEntity other = (ConfigurationEntity) obj;
+		if (active != other.active)
+			return false;
+		if (appName == null) {
+			if (other.appName != null)
+				return false;
+		} else if (!appName.equals(other.appName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type != other.type)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
 }
